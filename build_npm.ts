ac23188846ac288437fd3d6f0df2f57c8e3323dd
marmooo <marmooo@users.noreply.github.com>
2026-09-02 22:04:15 +0900

    0.0.4

--- build_npm.ts
+++ build_npm.ts
@@ -17,7 +17,7 @@ await build({
   },
   package: {
     name: "@marmooo/sf2-to-sf3",
-    version: "0.0.3",
+    version: "0.0.4",
     description: "Convert SF2 to SF3.",
     license: "MIT",
     repository: {
